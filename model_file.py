f35e88d3a5e9f2706f7b6ab4c3b5cbda1bda9667
sid21g <8859981+sid21g@users.noreply.github.com>
2021-11-01 22:59:32 +0530

    uniform lowercase

--- model_file.py
+++ model_file.py
@@ -2,7 +2,10 @@ import pandas as pd
 import re
 
 def get_images(dataset, user_input):
-    data_df_required = dataset.dropna()
+    data_df_required = dataset.dropna().copy()
     #user_input = " " + user_input + " "
+    user_input = user_input.lower()
+   # data_df_required
+    #data_df_required['transcript'] = data_df_required['transcript'].str.lower()  
     data_df_required = dataset.loc[dataset['transcript'].str.contains(fr'\b{user_input}\b')]
     return data_df_required["img"].tolist()
--- preprocess.py
+++ preprocess.py
@@ -3,4 +3,5 @@ import pandas as pd
 @st.cache
 def preprocess__(file_name):
     data_df = pd.read_json(file_name)
+    data_df['transcript'] = data_df['transcript'].str.lower()
     return data_df
