c26cdb60e1dfbd66a33b0b882ea6b9a6375af97f
sid21g <8859981+sid21g@users.noreply.github.com>
2021-11-01 21:53:44 +0530

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "siddharthgupta234@gmail.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
