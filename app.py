5bb9a1846e10a30cfe02dccf0e961e9ee90dff4a
Siddharth Gupta <8859981+sidgupta234@users.noreply.github.com>
2021-11-02 21:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ text-align: center;
 <div class="footer">
 <p>Developed by <a style='text-align: left;' href="https://www.languageof.me/" target="_blank">Siddharth Gupta</a> | 
 All image rights reserved to the very awesome <a style='text-align: center;' href="https://www.xkcd.com/" target="_blank">xkcd</a>
-Under <a style='text-align: center;' href="https://xkcd.com/license.html" target="_blank">Creative Commons License</a> | 
+Under <a style='text-align: center;' href="https://xkcd.com/license.html" target="_blank">Attribution-NonCommercial 2.5 License.</a> | 
 <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fxkcd-fetcher.herokuapp.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
 </p>
 </div>
